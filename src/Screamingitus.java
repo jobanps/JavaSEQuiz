6b1905d705995d0491a7619ba1e002a608e1c3f7
748529 <748529@123-PC42.student.Tor.Loc>
2019-05-21 16:58:52 -0400

    PART 1:  How many instructors are infected?

--- src/Screamingitus.java
+++ src/Screamingitus.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
+
+//Student Id : C0748529 - Jobanpreet Singh 
 
 public class Screamingitus {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter number of weeks to know infection rate for ");
+		int numberOfWeeks = input.nextInt();
+		int numberOfDays = numberOfWeeks * 7;
+		int infectedCount = 0;
+		
+		//Day 1 (1) + remaining days * rate(5)
+		infectedCount = 1 + (numberOfDays - 1) * 5;
+		
+		for(int i = 1 ; i <= numberOfWeeks ; i++) {
+			
+			if(i == 1) {
+				infectedCount = 1 + 30 ;	// Day 1 + remainingDays * 5
+			}
+			else {
+				infectedCount = infectedCount + 35; // infected + numbers_of_Days_In_a_Week * 5 (current week)
+			}
+			
+	
+			System.out.println("Number of instructors infected after week '" + i + "' : " + infectedCount);
+		}
+			
+		System.out.println("\nTotal Number of instructors infected after '" + numberOfWeeks +"' of weeks  : " + infectedCount);
+		
+		input.close();
+		
+		}
+		
 
 }
+
